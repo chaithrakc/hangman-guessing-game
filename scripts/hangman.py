5f55230fdda93c042813500acf085aa862161929
Chaithra K C <chaithra.kc22@gmail.com>
2021-07-05 02:29:23 +0530

    implementation

--- scripts/hangman.py
+++ scripts/hangman.py
@@ -1,7 +1,23 @@
 class Hangman:
-    def __init__(self, word_hint: tuple, hangmans: list):
+    def __init__(self, word_hint: tuple, hangmans: list, theme: str):
         self.word, self.hint = word_hint
+        self.encrypted_word = '_ ' * len(self.word)
         self.hangmans = hangmans
+        self.theme = theme
+        self.missed_letters = set()
+
+    # based on the probability of winning/losing, provide the hint for the word
+    def winning_probability(self) -> int:
+        pass
 
     def play(self):
-        pass
+        for hangman in self.hangmans:
+            self.display(hangman)
+            guess_word = input('Guess the letter or the entire word:')
+
+    def display(self, hangman):
+        lines = hangman.split('\n')
+        lines[2] = lines[2] + '\t\t\t       (' + self.theme + ')'
+        lines[3] = lines[3] + '\t\t\tMissed Letters: ' + ','.join(self.missed_letters)
+        lines[4] = lines[4] + '\t\t\tWord: ' + self.encrypted_word
+        print('\n'.join(lines))
--- scripts/main.py
+++ scripts/main.py
@@ -5,25 +5,31 @@ import random
 from hangman import Hangman
 
 
-def load_word_theme() -> dict:
-    resources = os.listdir('../resources')
-    theme_files = list(filter(lambda input_file: input_file.startswith('word_theme_'), resources))
-    random_theme = random.choice(theme_files)
-    relative_filepath = os.path.join('', '../resources', '../resources/'+random_theme)
-    with open(relative_filepath) as file_handler:
-        return json.load(file_handler)
+class Game:
 
+    def __init__(self):
+        self.themes = ['Animal', 'Movie', 'Place']
+        self.theme = None
 
-def load_hangman_pics() -> list:
-    relative_filepath = os.path.join('', '../resources', 'hangman_pics.txt')
-    with open(relative_filepath) as file_handler:
-        hangmans_pics = file_handler.read()
-        return hangmans_pics.split(',')
+    def load_word_theme(self) -> dict:
+        resources = os.listdir('../resources')
+        self.theme = random.choice(self.themes).lower()
+        file_name = list(filter(lambda input_file: input_file.find(self.theme) > -1, resources))
+        relative_filepath = os.path.join('', '../resources', '../resources/' + file_name[0])
+        with open(relative_filepath) as file_handler:
+            return json.load(file_handler)
+
+    def load_hangman_pics(self) -> list:
+        relative_filepath = os.path.join('', '../resources', 'hangman_pics.txt')
+        with open(relative_filepath) as file_handler:
+            hangmans_pics = file_handler.read()
+            return hangmans_pics.split(',')
 
 
 if __name__ == '__main__':
-    hangmans = load_hangman_pics()
-    theme = load_word_theme()
-    word_hint = random.choice(list(theme.items()))
-    hangman = Hangman(word_hint, hangmans)
+    game = Game()
+    hangmans = game.load_hangman_pics()
+    words = game.load_word_theme()
+    word_hint = random.choice(list(words.items()))
+    hangman = Hangman(word_hint, hangmans, game.theme)
     hangman.play()
