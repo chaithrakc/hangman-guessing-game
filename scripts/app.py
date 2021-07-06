3f86f84a2901adef98e4ae5638aab630c5e8053b
Chaithra K C <chaithra.kc22@gmail.com>
2021-07-06 23:54:13 +0530

    implementation

---
+++ scripts/app.py
@@ -0,0 +1,39 @@
+import json
+import os
+import random
+
+from hangman import Hangman
+
+
+class App:
+    word_them = None
+    WORD_THEME_ = 'word_theme_'
+
+    def set_theme(self, file_name: str):
+        start_index = file_name.index(self.WORD_THEME_) + len(self.WORD_THEME_)
+        end_index = file_name.index('.json')
+        self.word_them = file_name[start_index: end_index]
+
+    def load_word_theme(self) -> dict:
+        resources = os.listdir('../resources')
+        word_files = list(filter(lambda input_file: input_file.find(self.WORD_THEME_) > -1, resources))
+        file_name = random.choice(word_files)  # randomly selecting one of the theme
+        self.set_theme(file_name)
+        relative_filepath = os.path.join('', '../resources', '../resources/' + file_name)
+        with open(relative_filepath) as file_handler:
+            return json.load(file_handler)
+
+    def load_hangman_pics(self) -> list:
+        relative_filepath = os.path.join('', '../resources', 'hangman_pics.txt')
+        with open(relative_filepath) as file_handler:
+            hangmans_pics = file_handler.read()
+            return hangmans_pics.split(',')
+
+
+if __name__ == '__main__':
+    app = App()
+    hangmans = app.load_hangman_pics()
+    words = app.load_word_theme()
+    word_hint = random.choice(list(words.items()))  # randomly selecting a word withing a particular theme file
+    hangman = Hangman(word_hint, hangmans, app.word_them)
+    hangman.play()
--- scripts/hangman.py
+++ scripts/hangman.py
@@ -1,23 +1,39 @@
+import re
+
+
+# todo - check for words with spaces from input
 class Hangman:
     def __init__(self, word_hint: tuple, hangmans: list, theme: str):
         self.word, self.hint = word_hint
-        self.encrypted_word = '_ ' * len(self.word)
-        self.hangmans = hangmans
         self.theme = theme
+        self.sparse_word = list(' '.join(self.word))
+        self.blanks = list(re.sub('\\S+', '_', ' '.join(self.word)))
+        self.hangmans = hangmans
         self.missed_letters = set()
+        self.char_frame = dict()
+        for index, char in enumerate(self.sparse_word):
+            if char != ' ':
+                self.char_frame.setdefault(char, list()).append(index)
+
+    def play(self):
+        for chance in range(len(self.hangmans)):
+            self.display_hangman(self.hangmans[chance])
+            guess = input('Guess the letter or the entire word:')
+            self.match_the_guess(guess)
+
+    def match_the_guess(self, guess: str) -> bool:
+        char_indices = self.char_frame.get(guess)
+        for index in char_indices:
+            self.blanks[index] = guess
+        return True
+
+    def display_hangman(self, hangman):
+        lines = hangman.split('\n')
+        lines[2] = lines[2] + '\t\t\t\t\t(' + self.theme + ')'
+        lines[3] = lines[3] + '\t\t\tMissed Letters: ' + ','.join(self.missed_letters)
+        lines[4] = lines[4] + '\t\t\tWord: ' + str(self.blanks)
+        print('\n'.join(lines))
 
     # based on the probability of winning/losing, provide the hint for the word
     def winning_probability(self) -> int:
         pass
-
-    def play(self):
-        for hangman in self.hangmans:
-            self.display(hangman)
-            guess_word = input('Guess the letter or the entire word:')
-
-    def display(self, hangman):
-        lines = hangman.split('\n')
-        lines[2] = lines[2] + '\t\t\t       (' + self.theme + ')'
-        lines[3] = lines[3] + '\t\t\tMissed Letters: ' + ','.join(self.missed_letters)
-        lines[4] = lines[4] + '\t\t\tWord: ' + self.encrypted_word
-        print('\n'.join(lines))
